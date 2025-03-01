217c3f4951f991c5e9c4e18be6fe4200130540f9
Ednilson Graciano dos Santos <EddieSantos@users.noreply.github.com>
2025-03-01 00:07:41 +0000

    Desafio Novato - Cadastro das Cartas

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,93 +2,97 @@
 
 int main() {
 
-    // Declarando as variáveis da Carta 1
-    int carta = 1; // Número de carta variavel que sera incrementada postriormente como contagem de cartas
+    // Declarando as variáveis da Carta 1 e 2
+    int carta1, populacao1, turiticos1,
+        carta2, populacao2, turiticos2
+        ;
 
-    char codigo[3] = "01";
+    char codigo1[4], codigo2[4];
 
-    char estado[2] = "A";
+    char estado1, estado2;
 
-    char cidade[] = "Contagem";
+    char cidade1[50], cidade2[50];
 
-    int populacao = 12000000;
-
-    float area = 1521.11;
-
-    float pib = 699000000000;
-
-    int turiticos = 12;
+    float area1, area2, pib1, pib2;
 
     printf("Bem vindo ao Super Trunfo\n \n"); // Mensagem de boas vindas
-    printf("Jogador 1, cadastre sua carta\n \n"); // Mensagem para o jogador 1 cadastrar a carta
+    printf("Jogador, cadastre sua carta 1\n \n"); // Mensagem para o jogador cadastrar a sua primeira carta
 
-    printf("Digite o codigo da carta: ");
-    scanf("%s", &codigo);
+    // Recebendo os dados da carta 1
+    printf("Digite o numero da carta: ");
+    scanf("%d", &carta1);
 
-    printf("Digite o estado: ");
-    scanf("%s", &estado);
+    printf("Digite o codigo (01-08) da carta: ");
+    scanf("%s", &codigo1);
 
-    printf("Digite a cidade: ");
-    scanf("%s", &cidade);
-
-    printf("Digite a populacao: ");
-    scanf("%d", &populacao);
-
-    printf("Digite a Area: ");
-    scanf("%f", &area);
-
-    printf("Digite o PIB: ");
-    scanf("%f", &pib);
-
-    printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &turiticos);
-
-    printf("Carta cadastrada com sucesso!\n \n"); // Mensagem de cadastro com sucesso
-
-    // Exibindo os dados da carta do jogador 1
-    printf("Dados da Carta Jogador 1\n \n");
-    printf("Codigo da Carta: %s%s\n", estado, codigo); 
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f Km²\n", area);
-    printf("PIB: %.2f Bilhões de reais\n", pib);
-    printf("Nº de Pontos Turisticos: %d\n \n", turiticos);
-
-    printf("Jogador 2, sua vez de cadastrar a sua carta\n \n"); // Mensagem para o jogador 2 cadastrar a sua carta
-
-    printf("Digite o codigo da carta: ");
-    scanf("%s", &codigo);
-
-    printf("Digite o estado: ");
-    scanf("%s", &estado);
+    printf("Digite o estado (A-H): ");
+    scanf("%c", &estado1); //nesse caso usa o %c para ler um caractere e não %s uma string
 
     printf("Digite a cidade: ");
-    scanf("%s", &cidade);
+    scanf("%s", &cidade1);
 
     printf("Digite a populacao: ");
-    scanf("%d", &populacao);
+    scanf("%d", &populacao1);
 
     printf("Digite a Area: ");
-    scanf("%f", &area);
+    scanf("%f", &area1);
 
     printf("Digite o PIB: ");
-    scanf("%f", &pib);
+    scanf("%f", &pib1);
 
     printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &turiticos);
+    scanf("%d", &turiticos1);
 
-    printf("Carta cadastrada com sucesso\n \n"); // Mensagem de cadastro com sucesso
+    printf("Carta 1 cadastrada com sucesso!\n \n"); // Mensagem de cadastro com sucesso
 
-    // Exibindo os dados da carta do jogador 2
-    printf("Dados da Carta Jogador 2\n \n");
-    printf("Codigo da Carta: %s%s\n", estado, codigo);
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f Km²\n", area);
-    printf("PIB: %.2f Bilhões de reais\n", pib);
-    printf("Nº de Pontos Turisticos: %d\n", turiticos);
+    // Exibindo os dados da carta 1
+    printf("Dados da Carta %d\n", carta1);
+    printf("Codigo da Carta: %c%s\n", estado1, codigo1); // nesse retora %c%s para concatenar um caractere com uma string
+    printf("Estado: %c\n", estado1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f Bilhões de reais\n", pib1);
+    printf("Nº de Pontos Turisticos: %d\n \n", turiticos1);
+
+    printf("Jogador, cadastre a sua carta 2\n \n"); // Mensagem para o jogador cadastrar a sua carta 2
+
+    // Recebendo os dados da carta 2
+    printf("Digite o numero da carta: ");
+    scanf("%d", &carta2);
+
+    printf("Digite o codigo (01-08) da carta: ");
+    scanf("%s", &codigo2);
+
+    printf("Digite o estado (A-H): ");
+    scanf("%c", &estado2); //nesse caso usa o %c para ler um caractere e não %s uma string
+
+    printf("Digite a cidade: ");
+    scanf("%s", &cidade2);
+
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Area: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &turiticos2);
+
+    printf("Carta 2 cadastrada com sucesso\n \n"); // Mensagem de cadastro com sucesso
+
+    // Exibindo os dados da carta 2
+    printf("Dados da Carta %d\n", carta2);
+    printf("Codigo da Carta: %c%s\n", estado2, codigo2); // nesse retora %c%s para concatenar um caractere com uma string
+    printf("Estado: %c\n", estado2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f Bilhões de reais\n", pib2);
+    printf("Nº de Pontos Turisticos: %d\n", turiticos2);
 
     return 0;
 }
